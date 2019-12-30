17c6139511f4fd0a1d3dfedee102e72705a9e556
Manuel Ferran Pousa <manolobiene@gmail.com>
2019-12-30 01:58:51 +0100

    Make sure there's always empty seats for the desired group size

--- worker.js
+++ worker.js
@@ -13,7 +13,7 @@ const {
   setViewPort
 } = require("taiko");
 
-const seatsToReserve = process.env.ASIENTOS || 9;
+const seatsToReserve = Number.parseInt(process.env.ASIENTOS) || 9;
 const movieTitle = process.env.PELICULA || "Star Wars: Episodio IX - El Ascenso de Skywalker";
 const roomType = process.env.TIPO_DE_SALA || "VOSE";
 const reservationDate = process.env.FECHA || "30/12";
@@ -22,30 +22,56 @@ const reservationTime = process.env.HORA || "21:20";
 async function hack() {
   while (true) {
     await goto("https://www.cinesa.es/cines/as-cancelas");
-    await click($('//article[@class="seleccionardia"]/div/div'));
-    await click(reservationDate);
+    await setReservationDate();
     await click(reservationTime, below(movieTitle), near(roomType));
     await click("Continuar y crear cuenta después");
 
     await waitFor(1000);
-    await click($('//*[@id="accordion"]/div[1]/div/div[1]'), { clickCount: seatsToReserve });
+    await setSeatsNumber(seatsToReserve);
     await click("Elegir butacas");
 
     const roomFull = await text("Lo sentimos, ya no quedan butacas").exists();
     if (roomFull) {
-      console.log("The room is full");
+      console.log("La sala está llena. Esperando a que caduquen las butacas bloqueadas");
     } else {
-      const emptySeats = (await $(".butaca_2.disponible").elements()).length;
-      const occupiedSeats = (await $(".butaca_5").elements()).length;
-      console.log(
-        `[${emptySeats}] out of [${emptySeats + seatsToReserve + occupiedSeats}] seats left`
-      );
-      if (emptySeats === 0) {
-        await click("Atrás");
+      let emptySeats = await getEmptySeats();
+      await click("Atrás");
+      if (emptySeats > 0) {
+        await setSeatsNumber(Math.min(emptySeats, 8));
+        await click("Elegir butacas");
       }
+      await logEmptySeats(seatsToReserve);
     }
     await closeTab();
   }
+}
+
+async function logEmptySeats() {
+  const occupiedSeats = await getOccupiedSeats();
+  const emptySeats = await getEmptySeats();
+  const selectedSeats = await getSelectedSeats();
+  console.log(`[${emptySeats}] butacas libres de [${emptySeats + selectedSeats + occupiedSeats}]`);
+}
+
+async function setReservationDate() {
+  await click($('//article[@class="seleccionardia"]/div/div'));
+  await click(reservationDate);
+}
+
+async function setSeatsNumber(number) {
+  await click($('//*[@id="accordion"]/div[1]/div/div[1]'), { clickCount: number });
+}
+
+async function getEmptySeats() {
+  return (await $(".butaca_2.disponible").elements()).length;
+}
+
+async function getOccupiedSeats() {
+  return (await $(".butaca_5").elements()).length;
+}
+
+async function getSelectedSeats() {
+  return (await $(".butaca_1.disponible").elements()).length;
 }
 
 async function recursiveStart() {
